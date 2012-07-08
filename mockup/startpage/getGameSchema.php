1ac01f60dad1941cc8daf9957ce24c760bb6e9cc
Marcin Bielak <marcin.bieli@gmail.com>
2012-07-08 19:22:51 +0200

    add grid for cards faster positioning

--- mockup/startpage/getGameSchema.php
+++ mockup/startpage/getGameSchema.php
@@ -16,142 +16,142 @@ if (isset($_GET['name'])
                 'desc' => 'Clone of popular card game made for multiplayer.',
                 'board' => array(
                     'size' => array(
-                        'x' => 7,
+                        'x' => 9,
                         'y' => 5
                     ),
                 ),
-            'cards_params' => array(
-                    'count' => 24,
-                    'imagesize' => array(
-                        'x' => 100,
-                        'y' => 150
-                    ),
-            ),
-            'cards' => array(
-                 array(
-                    'name' => 'start1',
-                    'metrics' => array(
-                        'W' => true,
-                        'S' => true,
-                        'N' => true,
-                        'E' => true
-                    ),
-                    'opts' => array(
-                        'start' => true,
-                    ),
+                'cards_params' => array(
+                        'count' => 24,
+                        'imagesize' => array(
+                            'x' => 100,
+                            'y' => 150
+                        ),
                 ),
-                array(
-                    'name' => 'finish1',
-                    'metrics' => array(
-                        'W' => true,
-                        'S' => true,
-                        'N' => true,
-                        'E' => true
+                'cards' => array(
+                     array(
+                        'name' => 'start1',
+                        'metrics' => array(
+                            'W' => true,
+                            'S' => true,
+                            'N' => true,
+                            'E' => true
+                        ),
+                        'opts' => array(
+                            'start' => true,
+                        ),
                     ),
-                    'opts' => array(
-                        'finish' => true,
-                        'destination' => true,
-                        'hidden' => true
+                    array(
+                        'name' => 'finish1',
+                        'metrics' => array(
+                            'W' => true,
+                            'S' => true,
+                            'N' => true,
+                            'E' => true
+                        ),
+                        'opts' => array(
+                            'finish' => true,
+                            'destination' => true,
+                            'hidden' => true
+                        ),
                     ),
-                ),
-                array(
-                    'name' => 'finish2',
-                    'metrics' => array(
-                        'W' => true,
-                        'S' => false,
-                        'N' => false,
-                        'E' => true
+                    array(
+                        'name' => 'finish2',
+                        'metrics' => array(
+                            'W' => true,
+                            'S' => false,
+                            'N' => false,
+                            'E' => true
+                        ),
+                        'opts' => array(
+                            'finish' => true,
+                            'destination' => false,
+                            'hidden' => true
+                        ),
                     ),
-                    'opts' => array(
-                        'finish' => true,
-                        'destination' => false,
-                        'hidden' => true
+                    array(
+                        'name' => 'finish3',
+                        'metrics' => array(
+                            'W' => false,
+                            'S' => false,
+                            'N' => true,
+                            'E' => true
+                        ),
+                        'opts' => array(
+                            'finish' => true,
+                            'destination' => false,
+                            'hidden' => true
+                        ),
                     ),
-                ),
-                array(
-                    'name' => 'finish3',
-                    'metrics' => array(
-                        'W' => false,
-                        'S' => false,
-                        'N' => true,
-                        'E' => true
+                    array(
+                        'name' => 'card1',
+                        'metrics' => array(
+                            'W' => false,
+                            'S' => false,
+                            'N' => true,
+                            'E' => true
+                        ),
+                        'opts' => array(
+                        ),
                     ),
-                    'opts' => array(
-                        'finish' => true,
-                        'destination' => false,
-                        'hidden' => true
+                    array(
+                        'name' => 'card2',
+                        'metrics' => array(
+                            'W' => false,
+                            'S' => true,
+                            'N' => true,
+                            'E' => true
+                        ),
+                        'opts' => array(
+                        ),
                     ),
-                ),
-                array(
-                    'name' => 'card1',
-                    'metrics' => array(
-                        'W' => false,
-                        'S' => false,
-                        'N' => true,
-                        'E' => true
+                    array(
+                        'name' => 'card3',
+                        'metrics' => array(
+                            'W' => true,
+                            'S' => true,
+                            'N' => true,
+                            'E' => true
+                        ),
+                        'opts' => array(
+                        ),
                     ),
-                    'opts' => array(
-                    ),
-                ),
-                array(
-                    'name' => 'card2',
-                    'metrics' => array(
-                        'W' => false,
-                        'S' => true,
-                        'N' => true,
-                        'E' => true
-                    ),
-                    'opts' => array(
-                    ),
-                ),
-                array(
-                    'name' => 'card3',
-                    'metrics' => array(
-                        'W' => true,
-                        'S' => true,
-                        'N' => true,
-                        'E' => true
-                    ),
-                    'opts' => array(
-                    ),
-                ),
 
 
-            ),
-            'locations' => array(
-                    'start' => array(
-                        'count' => 1,
-                        'method' => 'none',
-                        'prefix' => 'startcard',
-                        'collection' => array(
-                            1 => array(
-                                'x' => 3,
-                                'y' => 1
+                ),
+                'locations' => array(
+                        'start' => array(
+                            'count' => 1,
+                            'method' => 'none',
+                            'prefix' => 'startcard',
+                            'collection' => array(
+                                1 => array(
+                                    'x' => 3,
+                                    'y' => 1
+                                ),
+                            ),
+                        ),
+                        'finish' => array(
+                            'count' => 3,
+                            'method' => 'rand',
+                            'prefix' => 'finishcard',
+                            'collection' => array(
+                                1 => array(
+                                    'x' => 1,
+                                    'y' => 7
+                                ),
+                                2 => array(
+                                    'x' => 3,
+                                    'y' => 7
+                                ),
+                                3 => array(
+                                    'x' => 7,
+                                    'y' => 7
+                                ),
                             ),
                         ),
                     ),
-                    'finish' => array(
-                        'count' => 3,
-                        'method' => 'rand',
-                        'prefix' => 'finishcard',
-                        'collection' => array(
-                            1 => array(
-                                'x' => 1,
-                                'y' => 7
-                            ),
-                            2 => array(
-                                'x' => 3,
-                                'y' => 7
-                            ),
-                            3 => array(
-                                'x' => 7,
-                                'y' => 7
-                            ),
-                        ),
-                    ),
-                ),
+                )
             )
-        )
     );
 
 else:
